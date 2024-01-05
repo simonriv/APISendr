6e0345c46a1b75078cfccf7f65c5392a51058063
Simon Rivera <simon.rivera.a@outlook.com>
2024-01-05 10:29:09 -0500

    creation of function for request by file

--- apisendr.py
+++ apisendr.py
@@ -1,5 +1,5 @@
 import click
-import requests
+import utils
 
 @click.group()
 def APISendr():
@@ -10,30 +10,30 @@ def APISendr():
 @click.option('--url','-u',required=True,help='Url for send request')
 def query(method,url):
     if method != None and url != None:
-        r = ''
-        if method == 'get':
-            try:
-                r = requests.get(url)
-            except requests.exceptions.HTTPError as errh:
-                raise errh
-            except requests.exceptions.ConnectionError as errc:
-                raise errc
-            except requests.exceptions.Timeout as errt:
-                raise errt
-            except requests.exceptions.RequestException as err:
-                raise err
+        r = utils.send_request(method,url,None,None)
+        if r.status_code == 200:
+            print(r.json())
+        else:
+            print(r.status_code)
+    else:
+        print('ERR/(0x000): Url and Method is required.')
+
+@APISendr.command()
+@click.option('--path','-p',required=True,help='Path file to Request')
+def file(path):
+    if path != None:
+        data = utils.read_json(path)
+        if utils.extract_data(data) != False:
+            method,url,body,headers = utils.extract_data(data)
+            r = utils.send_request(method,url,body,headers)
             if r.status_code == 200:
                 print(r.json())
             else:
                 print(r.status_code)
         else:
-            print('WAR/(0X000): Only use method get in query instruction')
+            print('ERR/(0x004): The file should not be empty')
     else:
-        print('ERR/(0x000): Url and Method is required.')
-
-@APISendr.command()
-def file():
-    print('API Send Request file')
+        print('ERR/(0x001): Path is required.')
 
 if __name__ == '__main__':
     APISendr()
---
+++ utils.py
@@ -0,0 +1,85 @@
+import json
+import os
+import requests
+
+def read_json(route):
+    with open(route, 'r') as f:
+        data = json.load(f)
+    return data
+
+def send_request(method,url,body,headers):
+    try:
+        if method == 'get' or method == 'GET':
+            if headers != None and body != None:
+                r = requests.get(url,data=body,headers=headers)
+            elif body != None and headers == None:
+                r = requests.get(url,data=body)
+            elif headers != None and body == None:
+                r = requests.get(url,headers=headers)
+            else:
+                r = requests.get(url)
+        elif method == 'post' or method == 'POST':
+            if headers != None and body != None:
+                r = requests.post(url,data=body,headers=headers)
+            elif body != None and headers == None:
+                r = requests.post(url,data=body)
+            elif headers != None and body == None:
+                r = requests.post(url,headers=headers)
+            else:
+                r = requests.post(url)
+        elif method == 'put' or method == 'PUT':
+            if headers != None and body != None:
+                r = requests.put(url,data=body,headers=headers)
+            elif body != None and headers == None:
+                r = requests.put(url,data=body)
+            elif headers != None and body == None:
+                r = requests.put(url,headers=headers)
+            else:
+                r = requests.put(url)
+        elif method == 'patch' or method == 'PATCH':
+            if headers != None and body != None:
+                r = requests.patch(url,data=body,headers=headers)
+            elif body != None and headers == None:
+                r = requests.patch(url,data=body)
+            elif headers != None and body == None:
+                r = requests.patch(url,headers=headers)
+            else:
+                r = requests.patch(url)
+        elif method == 'delete' or method == 'DELETE':
+            if headers != None and body != None:
+                r = requests.delete(url,data=body,headers=headers)
+            elif body != None and headers == None:
+                r = requests.delete(url,data=body)
+            elif headers != None and body == None:
+                r = requests.delete(url,headers=headers)
+            else:
+                r = requests.delete(url)
+        else:
+            return 'ERR/(0x003): The Method is not recognized.'
+    except requests.exceptions.HTTPError as errh:
+        raise errh
+    except requests.exceptions.ConnectionError as errc:
+        raise errc
+    except requests.exceptions.Timeout as errt:
+        raise errt
+    except requests.exceptions.RequestException as err:
+        raise err
+    return r
+
+def extract_data(data):
+    if data != []:
+        if "method" in data and "url" in data:
+            if "body" in data and "headers" in data:
+                return data["method"],data["url"],data["body"],data["headers"]
+            elif not "headers" in data and "body" in data:
+                return data["method"],data["url"],data["body"],None
+            elif not "body" in data and "headers" in data:
+                return data["method"],data["url"],None,data["headers"]
+            else:
+                return data["method"],data["url"],None,None
+        else:
+            print('ERR/(0x002): Method and Url is required.')
+            return False
+    else:
+        print('ERR/(0x002): For extract data the file is should not be empty')
+        return False
